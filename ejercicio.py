91dcaecd77b0ce7912af38649c7a13dc47399ba7
Ronald Araque <ronald2180575@correo.uis.edu.co>
2021-01-22 14:07:49 -0500

    guardar mf6,objeto sin,tdis,ims y modelo de flujo de agua subterranea

--- ejercicio.py
+++ ejercicio.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import flopy
 
-name = "ejer_dip_01"
+name = "tutorial01_mf6"
 h1 = 100
 h2 = 90
 Nlay = 10
@@ -12,6 +12,18 @@ L = 400.0
 H = 50.0
 k = 1.0
 
+
 sim = flopy.mf6.MFSimulation(
     sim_name=name, exe_name="mf6", version="mf6", sim_ws="."
 )
+
+#Create the Flopy TDIS object 
+tdis = flopy.mf6.ModflowTdis(
+    sim, pname="tdis", time_units="DAYS", nper=1, perioddata=[(1.0, 1, 1.0)]
+)
+
+#
+ims = flopy.mf6.ModflowIms(sim, pname="ims", complexity="SIMPLE")
+
+model_nam_file = "{}.nam".format(name)
+gwf = flopy.mf6.ModflowGwf(sim, modelname=name, model_nam_file=model_nam_file)
